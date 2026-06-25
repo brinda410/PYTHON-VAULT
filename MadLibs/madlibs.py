2e3beb764de2351b8d03913a1319077fa8901db4
brinda410 <brindarswamy@gmail.com>
2026-06-25 20:18:04 +0530

    Add madlibs game

---
+++ MadLibs/madlibs.py
@@ -0,0 +1,14 @@
+noun=input("Enter noun(animal/bird):")
+pronoun=input("Enter pronoun:")
+adjective=input("Enter an adjective(describing word):")
+adjective1=input("Enter an adjective(describing word):")
+verb=input("Enter verb(action):")
+
+print(f"A {noun} guarded the entrance of the kingdom. ")
+print(f"{pronoun} carried a {adjective} sword into battle.")
+print(f"An {adjective1} forest surrounded the castle.")
+print(f"The hero {verb} the evil sorcerer")
+
+
+
+
